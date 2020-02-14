18933cf604243e20696dd807c212627f64a2bf19
Simon H Moore <simonhughdev@gmail.com>
2020-02-14 17:51:29 +0000

    Fixed: cli option all working.

--- src/export.bash
+++ src/export.bash
@@ -8,6 +8,7 @@
 prefix=~/
 password_store=~/.password-store    # location of password-store
 gpg_id="$(cat "$password_store/.gpg-id")"       # get gpg-id from password-store
+now="$(date '+%Y-%m-%d_%H-%M-%S_%P')"
 
 package() {
     tar -zcf - -C "$password_store" --exclude ".passbk" ./ 
@@ -19,8 +20,8 @@ encrypt() {
 
 
 case "$1" in
-    -p|--plain) shift; package > ./ ;;
-    -e|--encrypt) shift; package | encrypt > ./ ;;
-    *) package | encrypt > ./ ;;
+    -p|--plain) shift; package > "$PWD/pass.$now.tar.gz" ;;
+    -e|--encrypt) shift; package | encrypt > "$PWD/pass.$now.tar.gz.gpg" ;;
+    *) package | encrypt > "$PWD/pass.$now.tar.gz.gpg" ;;
 esac
 exit 0

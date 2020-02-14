8138a3eed01db253e1ca67af82d84e2438a22317
Simon H Moore <simonhughdev@gmail.com>
2020-02-14 17:38:55 +0000

    Wrote encrypt function.

--- src/export.bash
+++ src/export.bash
@@ -5,8 +5,14 @@
 #
 # 
 
+prefix=~/
 password_store=~/.password-store    # location of password-store
+gpg_id="$(cat "$password_store/.gpg-id")"       # get gpg-id from password-store
 
 package() {
     tar -zcf - -C "$password_store" --exclude ".passbk" ./ 
+}
+
+encrypt() {
+    gpg -e -r "$gpg_id"
 }
